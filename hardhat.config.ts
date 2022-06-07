2e35b1ae087d9ac97a1cd96c2bca061a7b2ed22e
DanielZlotin <6660239+DanielZlotin@users.noreply.github.com>
2022-06-07 19:24:58 +0300

    hardhatDefaultConfig

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,15 @@
 import "dotenv/config";
-import { HardhatUserConfig, task } from "hardhat/config";
+import { task } from "hardhat/config";
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-web3";
 import "@nomiclabs/hardhat-etherscan";
 import "hardhat-tracer";
 import "hardhat-gas-reporter";
 import "hardhat-spdx-license-identifier";
-import { account, bn18, erc20s, ether, networks, web3 } from "@defi.org/web3-candies";
+import { account, ether, web3 } from "@defi.org/web3-candies";
 import { deploy } from "@defi.org/web3-candies/dist/hardhat/deploy";
+import { hardhatDefaultConfig } from "@defi.org/web3-candies/dist/hardhat";
+import _ from "lodash";
 
 task("deploy").setAction(async () => {
   console.log("deploying example Greeter contract:");
@@ -18,76 +20,4 @@ task("deploy").setAction(async () => {
   await deploy("Greeter", ["Hello, world!"], 5_000_000, 0, true, 1);
 });
 
-process.env.NETWORK = process.env.NETWORK?.toUpperCase() || "ETH";
-console.log(`🌐 network`, process.env.NETWORK, "blocknumber", process.env.BLOCK, "🌐");
-
-export default {
-  solidity: {
-    version: "0.8.10",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-    },
-  },
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      forking: {
-        blockNumber: process.env.BLOCK ? parseInt(process.env.BLOCK!) : undefined,
-        url: (process.env as any)[`NETWORK_URL_${process.env.NETWORK}`] || "",
-      },
-      blockGasLimit: 10e6,
-      accounts: {
-        accountsBalance: bn18(10e6).toString(),
-      },
-    },
-    eth: {
-      chainId: networks.eth.id,
-      url: process.env.NETWORK_URL_ETH || "",
-    },
-    bsc: {
-      chainId: networks.bsc.id,
-      url: process.env.NETWORK_URL_BSC || "",
-    },
-    poly: {
-      chainId: networks.poly.id,
-      url: process.env.NETWORK_URL_POLY || "",
-    },
-    avax: {
-      chainId: networks.avax.id,
-      url: process.env.NETWORK_URL_AVAX || "",
-    },
-  },
-  typechain: {
-    outDir: "typechain-hardhat",
-    target: "web3-v1",
-  },
-  mocha: {
-    timeout: 180_000,
-    retries: 0,
-    bail: true,
-  },
-  gasReporter: {
-    currency: "USD",
-    coinmarketcap: process.env.COINMARKETCAP,
-    token: gasReporter().token,
-    gasPriceApi: gasReporter().url,
-    showTimeSpent: true,
-  },
-  etherscan: {
-    apiKey: process.env[`ETHERSCAN_${process.env.NETWORK}`],
-  },
-} as HardhatUserConfig;
-
-function gasReporter() {
-  switch (process.env.NETWORK) {
-    case "AVAX":
-      return { token: "AVAX", url: "https://api.snowtrace.io/api?module=proxy&action=eth_gasPrice" };
-    case "POLY":
-      return { token: "MATIC", url: "https://api.polygonscan.com/api?module=proxy&action=eth_gasPrice" };
-    default:
-      return {};
-  }
-}
+export default _.merge(hardhatDefaultConfig(), {});
